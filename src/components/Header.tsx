57b841ccab989f0b78c6418b242cc541d76def05
namJeongwan <njw1107@naver.com>
2023-05-03 16:23:11 +0900

    19.0 Manual Fetching

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,67 @@
-import { HStack, IconButton, Button, Box, useDisclosure, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import {
+  HStack,
+  IconButton,
+  Button,
+  Box,
+  useDisclosure,
+  useColorMode,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
 import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUp";
 
 export default function Header() {
-    const{ isOpen:isLoginOpen, 
-        onClose:onLoginClose, 
-        onOpen:onLoginOpen
-    } = useDisclosure();
+  const {
+    isOpen: isLoginOpen,
+    onClose: onLoginClose,
+    onOpen: onLoginOpen,
+  } = useDisclosure();
 
-    const{ isOpen:isSignUpOpen, 
-        onClose:onSignUpClose, 
-        onOpen:onSignUpOpen
-    } = useDisclosure();
+  const {
+    isOpen: isSignUpOpen,
+    onClose: onSignUpClose,
+    onOpen: onSignUpOpen,
+  } = useDisclosure();
 
-    const {
-        colorMode,
-        toggleColorMode,
-    } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
-    const logoColor = useColorModeValue("red.500", "red.200");
-    const Icon = useColorModeValue(FaMoon, FaSun)
+  const logoColor = useColorModeValue("red.500", "red.200");
+  const Icon = useColorModeValue(FaMoon, FaSun);
 
-    return <HStack py={5} px={10} borderBottomWidth={1}
-    justifyContent={"space-between"}>
-        <Box color={logoColor}>
-            <FaAirbnb size={"48"}/>
-        </Box>
-        <HStack spacing={"2"}>
-            <IconButton 
-            variant={"ghost"}
-            aria-label="Toggle Dark Mode" 
-            icon={<Icon/>}
-            onClick={toggleColorMode}></IconButton>
-            <Button onClick={onLoginOpen}>Log in</Button>
-            <Button colorScheme="red" onClick={onSignUpOpen}>Sign up</Button>
-        </HStack>
-        <LoginModal isOpen={isLoginOpen} onClose={onLoginClose}/>
-        <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose}/>
-    </HStack>
+  return (
+    <Stack
+      py={5}
+      px={40}
+      borderBottomWidth={1}
+      justifyContent={"space-between"}
+      direction={{
+        sm: "column",
+        md: "row",
+      }}
+      spacing={{
+        sm: 3,
+        md: 4,
+      }}
+    >
+      <Box color={logoColor}>
+        <FaAirbnb size={"48"} />
+      </Box>
+      <HStack spacing={"2"}>
+        <IconButton
+          variant={"ghost"}
+          aria-label="Toggle Dark Mode"
+          icon={<Icon />}
+          onClick={toggleColorMode}
+        ></IconButton>
+        <Button onClick={onLoginOpen}>Log in</Button>
+        <Button colorScheme="red" onClick={onSignUpOpen}>
+          Sign up
+        </Button>
+      </HStack>
+      <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
+      <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
+    </Stack>
+  );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,76 @@
-export default function Home(){
-    return <span> Some Home </span>
+import { Grid } from "@chakra-ui/react";
+import Room from "../components/Room";
+import RoomSkeleton from "../components/RoomSkeleton";
+import { useEffect, useState } from "react";
+
+interface IPhoto {
+  pk: number;
+  file: string;
+  description: string;
+}
+
+interface IRoom {
+  pk: number;
+  name: string;
+  country: string;
+  city: string;
+  price: number;
+  rating: number;
+  is_owner: boolean;
+  photos: IPhoto[];
+}
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const fetchRooms = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
+    const json = await response.json();
+    // console.log(json);
+    setRooms(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+  return (
+    <Grid
+      mt={10}
+      px={{
+        base: 10,
+        lg: 40,
+      }}
+      columnGap={4}
+      rowGap={10}
+      templateColumns={{
+        sm: "1fr",
+        md: "repeat(2, 1fr)",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
+    >
+      {isLoading ? (
+        <>
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+        </>
+      ) : null}
+
+      {rooms.map((room) => (
+        <Room
+          imageUrl={room.photos[0].file}
+          name={room.name}
+          city={room.city}
+          price={room.price}
+          country={room.country}
+          rating={room.price}
+        />
+      ))}
+    </Grid>
+  );
 }
